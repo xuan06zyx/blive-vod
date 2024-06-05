720a2db5545bc4d6cc2251c1fcbcede0dbdc60f0
XuanO.o? <126635659+xuan06zyx@users.noreply.github.com>
2024-06-06 01:20:46 +0800

    1.0.1
    新增点歌屏蔽词、快捷启动

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ for _ in get_barrage:
     barrage_list = barrage_["barrage_list"]
     barrage_text_list = barrage_["barrage_text_list"]
     if barrage_text_list:
+        # 正则表达式
         diange = re.search(r'^点歌\s+(.*)', barrage_text_list[-1])
         if diange and last_song != diange.group(1) and diange.group(1):
-            # 正则表达式
             song_name = diange.group(1)  # 匹配点歌关键词
             last_song = song_name
             if song_name in BlackSong_list:
