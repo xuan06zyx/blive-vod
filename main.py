f94a6c4902bfe7e2b56739bfde64742938cff9aa
XuanO.o? <126635659+xuan06zyx@users.noreply.github.com>
2024-06-11 19:12:30 +0800

    2.0
    1.改为ws协议的实时弹幕接口
    2.优化版本管理代码

--- main.py
+++ main.py
@@ -1,85 +1,105 @@
-import get_barrage
-import questionary
-import webbrowser
-import requests
-import lxmusic
-import json
-import sys
 import re
+import sys
+import json
+import aiohttp
+import asyncio
+import webbrowser
+import http.cookies
+from typing import *
 
-version = 1.02
-# 版本管理
-github_url = 'https://api.github.com/repos/xuan06zyx/blive-vod/releases/latest'
-gitee_url = 'https://gitee.com/api/v5/repos/zyXuan06/blive-vod/releases/latest'
-# 读取配置文件
+import version
+import lxmusic
+import blivedm
+import blivedm.models.web as web_models
+
+# 打开配置文件
 with open('config.json', 'r', encoding='utf-8') as r:
     config = json.load(r)
-    release_api = config['release_api']
-    # 判断版本管理渠道
-    if release_api == '':
-        release_api = questionary.select("选择版本管理渠道", ['Github(Global)', 'Gitee(China)']).ask()
-        with open('config.json', 'w', encoding='utf-8') as w:
-            config['release_api'] = release_api
-            json.dump(config, w)
+version.version(config['version'], config['release_api'])  # 调用版本检查方法
+lxmusic = lxmusic.lxmusic()  # 实例化lxmusic类
 
-    if release_api == 'Github(Global)':
-        release_url = github_url
-    elif release_api == 'Gitee(China)':
-        release_url = gitee_url
-res = requests.get(release_url)
-latest_version = res.json()['name']
-if float(version) < float(latest_version):
-    print(
-        f'有可用更新,当前版本为{version},最新版本为{latest_version}\n请前往 {release_url} 下载最新版')
-else:
-    print(f'当前版本为最新版:{version}({release_api})')
-
+# 获取命令行参数
 if len(sys.argv) > 1:
-    roomid = sys.argv[1]
+    roomid = sys.argv[1]  # 位置为1的参数 0是本程序
+elif config['roomid'] != '':
+    roomid = config['roomid']  # 配置文件中存在roomid则使用配置文件中的roomid
 else:
-    roomid = input("请输入B站直播间号(回车确认):")
-get_barrage = get_barrage.Get_Barrage(roomid)
-lxmusic = lxmusic.lxmusic()
+    roomid = input("请输入B站直播间号(回车确认):")  # 没有则执行手动输入
 
-timeline = ''
-barrage_list = []
-barrage_text_list = []
-last_song = ''
-song_list = []
-BlackSong_list = []
+BlackSong_list = []  # 定义一个空列表用于存储屏蔽词
 # 打开黑名单文件并读取内容
-with open('.A歌曲黑名单.txt', 'r', encoding='utf-8') as Blacklist:
-    Blacklist_lines = Blacklist.readlines()
+with open('.A歌曲黑名单.txt', 'r', encoding='utf-8') as Blacklist:  # utf-8编码
+    Blacklist_lines = Blacklist.readlines()  # 读取所有行
 for line in Blacklist_lines:
     BlackSong = line.strip()  # 去除行末的换行符和多余的空格
-    BlackSong_list.append(BlackSong)
+    BlackSong_list.append(BlackSong)  # 添加到列表
 
-for _ in get_barrage:
-    # 弹幕列表变化以后触发迭代并调用方法
-    barrage_ = next(get_barrage)
-    admin = barrage_["admin"]
-    barrage_name = barrage_["barrage_name"]
-    barrage_list = barrage_["barrage_list"]
-    barrage_text_list = barrage_["barrage_text_list"]
-    if barrage_text_list:
+# 这里填一个已登录账号的cookie的SESSDATA字段的值。不填也可以连接，但是收到弹幕的用户名会打码，UID会变成0
+SESSDATA = ''
+
+session: Optional[aiohttp.ClientSession] = None
+
+
+async def main():
+    init_session()
+    try:
+        await run_single_client(room_id=int(roomid))
+    finally:
+        await session.close()
+
+
+def init_session():
+    cookies = http.cookies.SimpleCookie()
+    cookies['SESSDATA'] = SESSDATA
+    cookies['SESSDATA']['domain'] = 'bilibili.com'
+
+    global session
+    session = aiohttp.ClientSession()
+    session.cookie_jar.update_cookies(cookies)
+
+
+async def run_single_client(room_id: int):
+    """
+    监听一个直播间
+    """
+    room_id = room_id
+    client = blivedm.BLiveClient(room_id)
+    handler = MyHandler()
+    client.set_handler(handler)
+
+    client.start()
+    try:
+        await client.join()
+    finally:
+        await client.stop_and_close()
+
+
+class MyHandler(blivedm.BaseHandler):
+    # # 演示如何添加自定义回调
+    # _CMD_CALLBACK_DICT = blivedm.BaseHandler._CMD_CALLBACK_DICT.copy()
+
+    def _on_danmaku(self, client: blivedm.BLiveClient, message: web_models.DanmakuMessage):
+        msg = message.msg  # 弹幕内容
+        uname = message.uname  # 用户名
+        admin = message.admin  # 是否房管 0:否 1:是
+        print(f'[{message.timestamp}] {uname}：{msg}')
+
         # 弹幕切歌
-        if len(admin) > 0 and admin[-1]["text"] == '下一首' and admin[-1]["timeline"] != timeline:
-            print(f'收到切歌请求, 发送者:{admin[-1]["nickname"]}')
-            timeline = admin[-1]["timeline"]
-            webbrowser.open(url='lxmusic://player/skipNext')
+        if msg == '下一首' and admin == 1:  # 限房管
+            lxmusic.player_skipNext()  # 切换下一首
 
         # 弹幕点歌
-        # 正则表达式
-        diange = re.search(r'^点歌\s+(.*)', barrage_text_list[-1])
-        if diange and last_song != diange.group(1) and diange.group(1):
-            song_name = diange.group(1)  # 匹配点歌关键词
-            last_song = song_name
+        diange = re.match(r'点歌\s+(\S+)(?:\s+(\S+))?', msg)
+        if diange:
+            song_name = diange.group(1)  # 匹配的歌名
+            song_singer = diange.group(2) if diange.group(2) else ""  # 匹配的歌手 如果没有指定则为空
+
             # 判断屏蔽词
             if song_name in BlackSong_list:
-                print(f'发现屏蔽词:{song_name},发送者:{barrage_name}')
-                continue
-            print('收到点歌请求:', song_name)
-            Scheme_url = lxmusic.music_searchPlay(name=song_name, playLater=True)  # 调用封装好的落雪音乐模块
+                print(f'发现屏蔽词:{song_name},发送者:{uname}')
+            print(f'收到点歌请求: {song_name} 歌手:{song_singer}')
+            Scheme_url = lxmusic.music_searchPlay(name=song_name, singer=song_singer, playLater=True)  # True表示不立即播放
             webbrowser.open(url=Scheme_url)  # 使用webbrowser打开Scheme URL
 
-            song_list.append(song_name)  # 写入歌单列表
+
+asyncio.run(main())
